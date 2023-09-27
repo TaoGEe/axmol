5429a1724766b13da755afb1eb223e50cf426b4c
pkg_pc <pkg@bole.com>
2023-09-27 09:47:33 +0800

    'update'

---
+++ extensions/scripting/lua-bindings/manual/crypt/lua-crypt.h
@@ -0,0 +1,14 @@
+#ifndef __lua_crypt_h__
+#define __lua_crypt_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "tolua++.h"
+#ifdef __cplusplus
+}
+#endif
+
+int luaopen_crypt(lua_State* tolua_S);
+
+#endif // __lua_crypt_h__
--- extensions/scripting/lua-bindings/manual/lua_module_register.cpp
+++ extensions/scripting/lua-bindings/manual/lua_module_register.cpp
@@ -37,13 +37,16 @@
 #include "scripting/lua-bindings/manual/physics3d/axlua_physics3d_manual.h"
 #include "scripting/lua-bindings/manual/navmesh/axlua_navmesh_manual.h"
 #include "scripting/lua-bindings/manual/fairygui/lua_fairygui.hpp"
+extern "C" {
+#include "scripting/lua-bindings/manual/crypt/lua-crypt.h"
+}
 #include "lua_cjson.h"
 #include "yasio/bindings/yasio_axlua.hpp"
 
 static void lua_register_extensions(lua_State* L)
 {
 
-    static luaL_Reg lua_exts[] = {{"yasio", luaopen_yasio_axlua}, {"cjson", luaopen_cjson}, {NULL, NULL}};
+    static luaL_Reg lua_exts[] = {{"yasio", luaopen_yasio_axlua}, {"cjson", luaopen_cjson}, {"crypt", luaopen_crypt}, {NULL, NULL}};
 
     lua_getglobal(L, "package");
     lua_getfield(L, -1, "preload");
